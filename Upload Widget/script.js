1b13ce1a5cf42f8e76e5fc4baaca185405ebee96
Gautam Sharma <126454702+Gautammsharma@users.noreply.github.com>
2025-12-25 23:14:44 +0530

    Add file upload button functionality

---
+++ Upload Widget/script.js
@@ -0,0 +1,6 @@
+const uploadBtn = document.getElementById("uploadBtn");
+const fileInput = document.getElementById("fileInput");
+
+uploadBtn.addEventListener("click", () => {
+  fileInput.click();
+});
